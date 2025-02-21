96e581e9e7fc2167a8c71ac35205264012bde5fc
Abel1abel2 <138971478+Abel1abel2@users.noreply.github.com>
2025-02-21 01:08:20 -1000

    3d car view

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NavBar from './component/NavBar'
+import Car1 from './component/Car1'
+
+const App = () => {
+  return (
+    <>
+    <NavBar /> 
+    <div className='app'>
+    <Car1 />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,13 @@
+
+import '../App.css'
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+     
+    </div>
+  )
+}
+
+export default NavBar
